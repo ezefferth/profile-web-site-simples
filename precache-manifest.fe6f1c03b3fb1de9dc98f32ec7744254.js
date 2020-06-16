6d15a3955e266de6c2f682c7f39048de088e0b6c
ezefferth <ezefferth96@gmail.com>
2020-06-16 10:26:42 -0300

    Updates

---
+++ precache-manifest.fe6f1c03b3fb1de9dc98f32ec7744254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19707d0477d6ef55d69d8c0b861c9f76",
+    "url": "/profile-web-site-simples.git/index.html"
+  },
+  {
+    "revision": "b4fccaa17883a129bf64",
+    "url": "/profile-web-site-simples.git/static/css/main.96b261bb.chunk.css"
+  },
+  {
+    "revision": "42b8bfee5db425cf4524",
+    "url": "/profile-web-site-simples.git/static/js/2.925bcc59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile-web-site-simples.git/static/js/2.925bcc59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fccaa17883a129bf64",
+    "url": "/profile-web-site-simples.git/static/js/main.bb0eb0da.chunk.js"
+  },
+  {
+    "revision": "03a4f2734ba865e4af15",
+    "url": "/profile-web-site-simples.git/static/js/runtime-main.5725a3d7.js"
+  },
+  {
+    "revision": "53e042bcc4625287349416ac4004496f",
+    "url": "/profile-web-site-simples.git/static/media/Trees-road.53e042bc.jpg"
+  }
+]);
